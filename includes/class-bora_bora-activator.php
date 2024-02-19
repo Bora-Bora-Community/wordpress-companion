dab4a5e5a5b19d5f87163c1fee785c3bcb25624d
Adrian <322735+adrian-schnell@users.noreply.github.com>
2024-02-19 23:10:56 +0000

    init plugin boiler plate

---
+++ includes/class-bora_bora-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://bora-bora.io
+ * @since      1.0.0
+ *
+ * @package    Bora_bora
+ * @subpackage Bora_bora/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Bora_bora
+ * @subpackage Bora_bora/includes
+ * @author     Bora-Bora <support@bora-bora.io>
+ */
+class Bora_bora_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-bora_bora-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://bora-bora.io
+ * @since      1.0.0
+ *
+ * @package    Bora_bora
+ * @subpackage Bora_bora/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Bora_bora
+ * @subpackage Bora_bora/includes
+ * @author     Bora-Bora <support@bora-bora.io>
+ */
+class Bora_bora_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
