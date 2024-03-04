c93641ca0157d8fa4eaaf36347c31727b8740d4d
Adrian <322735+adrian-schnell@users.noreply.github.com>
2024-03-04 23:06:08 +0000

    Feature/set cookie (#3)
    * refactor: create own files for each action
    
    * fix path to the session manager
    
    * fix session
    
    * set default length to 1h

--- includes/class-bb-user-login.php
+++ includes/class-bb-user-login.php
@@ -1,7 +1,23 @@
 <?php
 
+use BB\API\BB_Api_Client;
+use BB\Service\BB_Session_Manager;
+use BB\Service\BB_User_Manager;
+
 function bb_after_login($user_login, $user)
 {
+    $bbSessionManager = new BB_Session_Manager();
+    // session cookie exists and is valid
+    if ($bbSessionManager->checkTransientExistsAndIsValid()) {
+        ray('valid cookie exists. abort');
+
+        return;
+    }
+
+    // get the user details from the bora bora api
+    // and update the user meta data
+    // create a new session cookie. time frame length is BORA_BORA_SESSION_VALID_TIMEFRAME_IN_HOURS
+
     $bbClient = new BB_Api_Client();
     $boraBoraId = carbon_get_user_meta($user->ID, 'bora_bora_id');
 
@@ -15,6 +31,7 @@ function bb_after_login($user_login, $user)
     // update the user meta data with the new data
     (new BB_User_Manager)->updateUserData(userId: $user->ID, data: $userDetails);
     ray(['user data per api' => $userDetails]);
+    $bbSessionManager->setTransient($userDetails['subscription']['subscription_id']);
 }
 
 add_filter('wp_login', 'bb_after_login', 10, 2);
