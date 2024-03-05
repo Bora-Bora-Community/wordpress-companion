675d148dd621d8a1daceff5651849c08d2b53020
Adrian <322735+adrian-schnell@users.noreply.github.com>
2024-03-05 11:06:07 +0000

    fixes

--- includes/class-bb-user-login.php
+++ includes/class-bb-user-login.php
@@ -4,12 +4,18 @@ use BB\API\BB_Api_Client;
 use BB\Service\BB_Session_Manager;
 use BB\Service\BB_User_Manager;
 
-function bb_after_login($user_login, $user)
+/**
+ * @param $user_login
+ * @param $user
+ *
+ * @return void
+ */
+function bb_after_login($user_login, $user): void
 {
     $bbSessionManager = new BB_Session_Manager();
     // session cookie exists and is valid
-    if ($bbSessionManager->checkTransientExistsAndIsValid(role: 'bb_subscription')) {
-        return [];
+    if ($bbSessionManager->checkTransientExistsAndIsValid(role: 'bb_discord_role')) {
+        return;
     }
 
     // get the user details from the bora bora api
@@ -18,16 +24,15 @@ function bb_after_login($user_login, $user)
 
     $bbClient = new BB_Api_Client();
     $boraBoraId = carbon_get_user_meta($user->ID, 'bora_bora_id');
-    
+
     if ($boraBoraId == []) {
-        return [];
+        return;
     }
     $userDetails = $bbClient->loadUserDetails(boraBoraId: $boraBoraId);
 
     // update the user metadata with the new data
     (new BB_User_Manager)->updateUserData(userId: $user->ID, data: $userDetails);
-    $bbSessionManager->setTransient(role: 'bb_subscription', data: $userDetails['subscription']['subscription_id']);
-    $bbSessionManager->updateTransient(role: 'bb_discord_role', data: $userDetails['subscription']['discord_group']);
+    $bbSessionManager->setTransient(role: 'bb_discord_role', data: $userDetails['subscription']['discord_group']);
 }
 
-add_filter('wp_login', 'bb_after_login', 10, 2);
+add_filter(hook_name: 'wp_login', callback: 'bb_after_login', priority: 10, accepted_args: 2);
