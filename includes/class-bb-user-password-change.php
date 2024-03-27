0283241398daafafb62615c928274184747e5b7a
Adrian <322735+adrian-schnell@users.noreply.github.com>
2024-03-27 23:17:02 +0000

    Feature/report password change (#10)
    * implement new endpoint
    
    * use the hook to call the new endpoint
    
    * register new file

---
+++ includes/class-bb-user-password-change.php
@@ -0,0 +1,16 @@
+<?php
+
+use BB\API\BB_Api_Client;
+use BB\Service\BB_Session_Manager;
+use BB\Service\BB_User_Manager;
+
+function my_custom_password_change($user_id, $new_pass): void
+{
+    $user = wp_get_current_user();
+    $boraId = carbon_get_user_meta($user->ID, 'bora_bora_id');
+
+    // now report the new password to the bora bora api
+    (new BB_Api_Client())->updateCustomerPassword(boraBoraId: $boraId, newPassword: $new_pass);
+}
+
+add_action('check_passwords', 'my_custom_password_change', 10, 2);
