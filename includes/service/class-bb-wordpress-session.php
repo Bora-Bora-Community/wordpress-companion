c24668355e3f44f5be9205cbf73a288583550244
Adrian <322735+adrian-schnell@users.noreply.github.com>
2024-05-07 16:50:01 +0200

    Session Length (#19)
    * set session auth prio to 99
    
    * ...

--- includes/service/class-bb-wordpress-session.php
+++ includes/service/class-bb-wordpress-session.php
@@ -2,9 +2,16 @@
 
 use BB\enum\Setting;
 
-function bb_wordpress_session_length(): int
+/**
+ * @param  int  $expiration
+ * @param  int  $user_id
+ * @param  bool  $remember
+ *
+ * @return int session length
+ */
+function bb_wordpress_session_length(int $expiration, int $user_id, bool $remember): int
 {
-    return carbon_get_theme_option(Setting::SESSION_LENGTH) ?? 31536000;
+    return (int) carbon_get_theme_option(Setting::SESSION_LENGTH) ?? YEAR_IN_SECONDS;
 }
 
-add_filter('auth_cookie_expiration', 'bb_wordpress_session_length');
+add_filter('auth_cookie_expiration', 'bb_wordpress_session_length', 99, 3);
