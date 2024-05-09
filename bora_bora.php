ac569fdce3e3f31b8ffd52ec6af6e85249998b75
Adrian <322735+adrian-schnell@users.noreply.github.com>
2024-05-09 19:16:36 +0200

    pump to 1.1.0

--- bora_bora.php
+++ bora_bora.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Bora Bora
  * Plugin URI:        https://bora-bora.io
  * Description:       Bora Bora offers a complete solution for managing your community, from the subscription to the management of the users and their access to the content
- * Version:           1.0.9
+ * Version:           1.1.0
  * Author:            Bora Bora
  * Author URI:        https://bora-bora.io/
  * License:           GPL-2.0+
@@ -27,7 +27,7 @@ if (!defined('WPINC')) {
 /**
  * Currently plugin version.
  */
-const BORA_BORA_VERSION = '1.0.9';
+const BORA_BORA_VERSION = '1.1.0';
 
 /**
  * The name of the Plugin
@@ -39,7 +39,7 @@ const BORA_BORA_NAME = 'Bora Bora';
  * The base URL of Bora Bora API
  * @since 1.0.0
  */
-const BORA_BORA_API_BASE_URL = 'https://bora-bora.test/api/companion/';
+const BORA_BORA_API_BASE_URL = 'https://bora-bora.io/api/companion/';
 const WP_ENV = 'dev';
 
 /**
