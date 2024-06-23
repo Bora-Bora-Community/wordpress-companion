efcd55b1d8f698193bbe1e8f8aa3799afd095ea0
Adrian <322735+adrian-schnell@users.noreply.github.com>
2024-06-23 09:21:44 +0100

    update version number

--- bora_bora.php
+++ bora_bora.php
@@ -9,7 +9,7 @@
  * Plugin Name:       Bora Bora
  * Plugin URI:        https://bora-bora.io
  * Description:       Bora Bora offers a complete solution for managing your community, from the subscription to the management of the users and their access to the content
- * Version:           1.1.3
+ * Version:           1.1.4
  * Author:            Bora Bora
  * Author URI:        https://bora-bora.io/
  * License:           GPL-2.0+
@@ -27,7 +27,7 @@ if (!defined('WPINC')) {
 /**
  * Currently plugin version.
  */
-const BORABORAIO_VERSION = '1.1.3';
+const BORABORAIO_VERSION = '1.1.4';
 
 /**
  * The name of the Plugin
